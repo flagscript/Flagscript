4eb5805dbb94ff0dac748dddc6cf77ec545b9c6d
Greg Kaestle <flagscript@gmail.com>
2018-09-08 01:03:29 -0700

    * Flagscript.sln: * Flagscript.Test.csproj: Adding Flagscript.Test unit test project.
    * EntityTest.cs: Unit tests for the Flagscript.Data.Entity namespace.
    
    * TestingEntities.cs: Sample entity classes for unit tests.
    
    * Entity.cs: Fix namespace.

---
+++ Flagscript.Test/Data/Entity/TestingEntities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Flagscript.Data.Entity;
+
+namespace Flagscript.Test.Data.Entity
+{
+
+	/// <summary>
+	/// Test entity with <see cref="Guid"/> identifier.
+	/// </summary>
+	public class GuidTestEntity : Entity<Guid>
+	{
+	}
+
+	/// <summary>
+	/// Test entity with <see cref="int"/> identifier.
+	/// </summary>
+	public class IntTestEntity : Entity<int>
+	{
+	}
+
+}
