2819460e46013a55fe59b6b17ed5548ab39567d0
Greg Kaestle <greg.kaestle@deckers.com>
2018-09-07 23:57:13 -0700

    Initial check-in of module Flagscript

---
+++ Flagscript/Data/Entity/INamedEntity.cs
@@ -0,0 +1,22 @@
+﻿namespace Flagscript.Data.Entity
+{
+
+	/// <summary>
+	/// Base model tagging interface for all entities in the Flagscript Framework.
+	/// </summary>
+	/// <remarks>
+	/// As a convention, all entities in the Flagscript Framework have a name 
+	/// value. This should be unique in most contexts.
+	/// </remarks>
+	public interface INamedEntity
+	{
+
+		/// <summary>
+		/// Gets or sets the name identifier of the entity.
+		/// </summary>
+		/// <value>The name identifier of the entity..</value>
+		string Name { get; set; }
+
+	}
+
+}
---
+++ Flagscript/FlagscriptException.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Flagscript
+{
+
+	/// <summary>
+	/// Represents errors that occur during application execution specific to the 
+	/// Flagscript framework.
+	/// </summary>
+	public class FlagscriptException : Exception
+	{
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FlagscriptException"/> class.
+		/// </summary>
+		public FlagscriptException() : base()
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FlagscriptException"/> 
+		/// class with a specified error message.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		public FlagscriptException(string message) : base(message)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="FlagscriptException"/> 
+		/// class with a specified error message and a reference to the inner 
+		/// exception that is the cause of this exception.
+		/// </summary>
+		/// <param name="message">The error message that explains the reason for the exception.</param>
+		/// <param name="innerException">The exception that is the cause of the current exception, 
+		/// or a null reference (<c>Nothing</c> in Visual Basic) if no inner exception is specified.</param>
+		public FlagscriptException(string message, Exception innerException)
+			: base(message, innerException)
+		{
+		}
+
+	}
+
+}
